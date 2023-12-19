c9e8e920dbdc5913fc2d7837b93cbfd0487628c0
Neelam <neelammujawar12@gmail.com>
2023-12-19 13:46:17 +0530

    Add Contact

--- AddBook.java
+++ AddBook.java
@@ -32,9 +32,29 @@ public class AddBook {
 
     public static void main(String[] args) {
          System.out.println("Welcome to AddressBook");
-       
-        
         ArrayList<Contact> con = new ArrayList<Contact>();
+
+        public static void add_contact() {
+           // sc.nextLine();// to avoid /n issue in after taking integer as input
+            System.out.println("enter first name ");
+            String first_name = sc.nextLine();
+            System.out.println("enter last name ");
+            String last_name = sc.nextLine();
+            System.out.println("enter address ");
+            String address = sc.nextLine();
+            System.out.println("enter  city");
+            String city = sc.nextLine();
+            System.out.println("enter  state");
+            String state = sc.nextLine();
+            System.out.println("enter zip-code");
+            int zip = sc.nextInt();
+            //sc.nextLine();
+            System.out.println("enter phone number");
+            String phone_number = sc.nextLine();
+            System.out.println("enter email");
+            String email = sc.nextLine();
+            Contact cont = new Contact(first_name, last_name, address, city, state, zip, phone_number, email);///class ob created 
+             con.add(cont);//built in method 
          
     }
     }
